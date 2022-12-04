4184357e1eff247a6bd8be808f153f76013511ff
Rahul Arjun Tavnoji <rahultavnoji6@gmail.com>
2022-12-04 16:04:16 +0100

    Camp Cleanup Part 2

--- Day4/CampCleanup.py
+++ Day4/CampCleanup.py
@@ -1,20 +1,29 @@
-def subset(sections):
-    elf1, elf2 = sections.split(",")
-    starting_section_elf1, ending_section_elf1 = map(int, elf1.split("-"))
-    starting_section_elf2, ending_section_elf2 = map(int, elf2.split("-"))
-    if starting_section_elf1 <= starting_section_elf2 and ending_section_elf1 >= ending_section_elf2:
+def subset(elf1, elf2):
+    if elf1[0] <= elf2[0] and elf1[1] >= elf2[1]:
         return 1
-    elif starting_section_elf1 >= starting_section_elf2 and ending_section_elf1 <= ending_section_elf2:
+    elif elf1[0] >= elf2[0] and elf1[1] <= elf2[1]:
+        return 1
+    return 0
+
+
+def overlap(elf1, elf2):
+    if elf1[0] <= elf2[0] <= elf1[1]:
+        return 1
+    elif elf2[0] <= elf1[0] <= elf2[1]:
         return 1
     return 0
 
 
 def total_subset_pairs():
     subset_pairs = 0
+    overlap_pairs = 0
     with open("AssignmentPairs") as assignment:
         for sections in assignment:
-            subset_pairs += subset(sections.strip())
-    return subset_pairs
+            elf1, elf2 = sections.strip().split(",")
+            elf1, elf2 = list(map(int, elf1.split("-"))), list(map(int, elf2.split("-")))
+            subset_pairs += subset(elf1, elf2)
+            overlap_pairs += overlap(elf1, elf2)
+    return subset_pairs, overlap_pairs
 
 
 print(total_subset_pairs())
